2852e8a2542af52a85d277b1a904786df48ded7e
jomarx <jmaquinay@gmail.com>
2018-09-11 10:17:56 +0800

    update
    working version
    with buzzer and LED lights

--- qaGET/config.h
+++ qaGET/config.h
@@ -1,2 +1,4 @@
 char ssid[] = "kpi4mech";	//  your network SSID (name)
 char pass[] = "kpi4mech1";	// your network password
+//char ssid[] = "outsourcing1.25";	//  your network SSID (name)
+//char pass[] = "dbafe54321!";	// your network password
--- qaGET/qaGET.ino
+++ qaGET/qaGET.ino
@@ -13,10 +13,11 @@
   this example is in the public domain
  */
 #include <ArduinoHttpClient.h>
-#include <ESP8266WiFi.h>
+//#include <ESP8266WiFi.h>
+#include <WiFi.h>
 #include "config.h"
 
-char serverAddress[] = "192.168.143.23";  // server address
+char serverAddress[] = "192.168.143.220";  // server address
 int port = 80;
 
 WiFiClient wifi;
@@ -26,95 +27,155 @@ String response;
 int statusCode = 0;
 
 String contentType = "application/x-www-form-urlencoded";
+String clientResponse = "";
+//defines where the plant will ring
+String NotifNo = "1";
 
-int clientResponse = 0;
+const int relaOutput = 12;
+const int ledLight = 4;
+
+//watchdog timer
+const int wdtTimeout = 10000;  //time in ms to trigger the watchdog
+hw_timer_t *timer = NULL;
+
+void IRAM_ATTR resetModule() {
+  ets_printf("WDTreboot\n");
+  esp_restart_noos();
+}
 
 void setup() {
-Serial.begin(9600);
-// Connect to WPA/WPA2 network:
-Serial.print("Attempting to connect to Network named: ");
-wifiConnect();
+	Serial.begin(115200);
 
-// print the SSID of the network you're attached to:
-Serial.print("SSID: ");
-Serial.println(WiFi.SSID());
+	timer = timerBegin(0, 80, true);                  //timer 0, div 80
+	timerAttachInterrupt(timer, &resetModule, true);  //attach callback
+	timerAlarmWrite(timer, wdtTimeout * 1000, false); //set time in us
+	timerAlarmEnable(timer);                          //enable interrupt
 
-// print your WiFi shield's IP address:
-IPAddress ip = WiFi.localIP();
-Serial.print("IP Address: ");
-Serial.println(ip);
+	WiFi.mode(WIFI_STA);
+
+	// Connect to WPA/WPA2 network:
+	wifiConnect();
+
+	// print the SSID of the network you're attached to:
+	Serial.print("SSID: ");
+	Serial.println(WiFi.SSID());
+
+	// print your WiFi shield's IP address:
+	IPAddress ip = WiFi.localIP();
+	Serial.print("IP Address: ");
+	Serial.println(ip);
+
+	pinMode(relaOutput, OUTPUT);
+	pinMode(ledLight, OUTPUT);
 }
 
 void loop() {
+	timerWrite(timer, 0); //reset timer (feed watchdog)
+	digitalWrite(relaOutput, LOW);
+	digitalWrite(ledLight, HIGH);
+	
 	clientResponse = checkRingStatQuery();
-	
-	delayer(5);
-	
-	if (clientResponse > 0) {
+	Serial.print("clientResponse: ");
+	Serial.println(clientResponse);
+	digitalWrite(ledLight, LOW);
+	if (clientResponse != "") {
 		changeRingStatQuery(clientResponse);
+		timerWrite(timer, 0); //reset timer (feed watchdog)
 	} else {
 		Serial.println("no task available!");
+		digitalWrite(ledLight, HIGH);
+		timerWrite(timer, 0); //reset timer (feed watchdog)
 	}
 	
-	delayer(5);
+	delayer(1);
+	digitalWrite(ledLight, LOW);
+	delayer(1);
+	timerWrite(timer, 0); //reset timer (feed watchdog)
 }
 
-int checkRingStatQuery () {
+String checkRingStatQuery () {
 	Serial.println("making POST request to check ringStat");
+	timerWrite(timer, 0); //reset timer (feed watchdog)
+	String postData = "tp=1&NotifNo=";
+	postData += NotifNo;
+	//String postData = "tp=1&password=admin";
 	
-	String postData = "typer=1";
-	//String postData = "typer=1&password=admin";
-
-	client.post("/jm/qaquerytask.php", contentType, postData);
+	long loopTime = millis();
+	Serial.print("postData CheckRSQ: ");
+	Serial.println(postData);
+	client.post("/qaqtk.php", contentType, postData);
+	loopTime = millis() - loopTime;
+  
+	Serial.print("loop time is = ");
+	Serial.println(loopTime); //should be under 3000
 
 	// read the status code and body of the response
 	statusCode = client.responseStatusCode();
 	response = client.responseBody();
+	
+	timerWrite(timer, 0); //reset timer (feed watchdog)
 
-	Serial.print("Status code: ");
+	Serial.print("checkRingStatQuery-Status code: ");
 	Serial.println(statusCode);
-	Serial.print("Response: ");
+	Serial.print("checkRingStatQuery-Response: ");
 	Serial.println(response);
 
 	//disconnect client
 	client.stop();
 
-	Serial.println("Wait five seconds");
-	delayer(5);
+	Serial.println("Wait 1 seconds");
+	delayer(2);
 	
 	if (response != "") {
-		clientResponse = response.indexOf(',');
-		Serial.println("summoning found, beeping!");
+		int firstCommaIndex = response.indexOf(',');
+		clientResponse = response.substring(0, firstCommaIndex);
+		Serial.println("summoning found, beeping!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
 		buzzerFunction(3);
+		timerWrite(timer, 0); //reset timer (feed watchdog)
 	} else {
 		//
-		Serial.println("no task found, checking again after 60 secs!");
-		delayer(60);
+		Serial.println("no task found, checking again after 5 secs!");
+		client.stop();
+		delayer(10);
+		timerWrite(timer, 0); //reset timer (feed watchdog)
 	}
-	
+	Serial.print("clientResponse: ");
+	Serial.println(clientResponse);
 	return clientResponse;
 }
 
-int changeRingStatQuery (int num) {
+String changeRingStatQuery (String num) {
+	timerWrite(timer, 0); //reset timer (feed watchdog)
 	Serial.println("making POST request to set ringStat to zero");
-	String postData = "typer=2&taskID=";
+	String postData = "tp=2&tid=";
 	postData += num;
-	Serial.print("postData: ");
+	postData += "&NotifNo=";
+	postData += NotifNo;
+	Serial.print("postData ChangeRSQ: ");
 	Serial.println(postData);
-
-	client.post("/jm/qaquerytask.php", contentType, postData);
-
+	
+	long loopTime = millis();
+	client.post("/qaqtk.php", contentType, postData);
+	timerWrite(timer, 0); //reset timer (feed watchdog)
+	loopTime = millis() - loopTime;
+  
+	Serial.print("loop time is = ");
+	Serial.println(loopTime); //should be under 10000
+	
 	// read the status code and body of the response
 	statusCode = client.responseStatusCode();
 	response = client.responseBody();
 
-	Serial.print("Status code: ");
+	Serial.print("changeRingStatQuery-Status code: ");
 	Serial.println(statusCode);
-	Serial.print("Response: ");
+	Serial.print("changeRingStatQuery-Response: ");
 	Serial.println(response);
 
 	//disconnect client
 	client.stop();
+	delay(800);
+	//clientResponse = "";
+	ESP.restart();
 	//
 }
 
@@ -122,10 +183,13 @@ int buzzerFunction(int counter){
 	for (int buzzerTimer = 1; buzzerTimer <= counter; buzzerTimer++){
 		//set buzzer on
 		Serial.println("BUZZ ON!");
-		delayer(1);
-		//set buzzer off
+		digitalWrite(relaOutput, HIGH);
+		digitalWrite(ledLight, HIGH);
+		delay(800);		//set buzzer off
 		Serial.println("BUZZ OFF!");
-		delayer(1);
+		digitalWrite(relaOutput, LOW);
+		digitalWrite(ledLight, LOW);
+		delay(800);
 		
 	}
 }
@@ -134,13 +198,29 @@ int delayer(int dly){
 	Serial.print("Delaying ");
 	Serial.print(dly);
 	Serial.print(" secs : ");
+	int ledStatus = 0;
 	for (int DelayDaw = 1; DelayDaw <= dly; DelayDaw++){
-		delay(500);
-		yield();
-		delay(500);
+		delay(800);
 		Serial.print(DelayDaw);
 		Serial.print(".");
+		timerWrite(timer, 0); //reset timer (feed watchdog)
+		int skipper = 0;
+		
+		if ((ledStatus == 1)&&(skipper == 0)) {
+			//lights off
+			digitalWrite(ledLight, LOW);
+			ledStatus = 0;
+			skipper = 1;
+		}
+		
+		if ((ledStatus == 0)&&(skipper == 0)) {
+			//lights on
+			digitalWrite(ledLight, HIGH);
+			ledStatus = 1;
+			skipper = 1;
+		}
 	}
+	
 	Serial.println("");
 }
 
@@ -149,15 +229,14 @@ void wifiConnect () {
 	WiFi.begin(ssid, pass);
 	int ResetCounter = 0;
 	Serial.print("Attempting to connect to Network ");
-	Serial.print("Connecting to ");
-	Serial.println(ssid);
 	while ( WiFi.status() != WL_CONNECTED) {
-		ESP.wdtFeed();
+		
 		Serial.print(".");
+		timerWrite(timer, 0); //reset timer (feed watchdog)
 		ResetCounter++;
-		yield();
+		
 		delay(300);
-		if (ResetCounter >= 60) {
+		if (ResetCounter >= 30) {
 			Serial.print("ESP8266 reset!");
 			ESP.restart();
 		}
